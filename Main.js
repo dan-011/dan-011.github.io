d4c62ebf4bfc73db65d666a6fb47f35f22e85c49
dan-011 <daniel.paliulis@uconn.edu>
2022-11-12 18:29:07 -0500

    Timeline mk4

--- Main.js
+++ Main.js
@@ -1,28 +1,60 @@
 function gotoGithub() {
-    window.open("https://github.com/dan-011");
+  window.open("https://github.com/dan-011");
 }
 function gotoLinkedIn() {
-    window.open("https://www.linkedin.com/in/daniel-paliulis-152620251/");
+  window.open("https://www.linkedin.com/in/daniel-paliulis-152620251/");
 }
+   
+"use strict";
 
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+function qs(selector, all = false) {
+  return all ? document.querySelectorAll(selector) : document.querySelector(selector);
 }
-
-var items = document.querySelectorAll(".timeline li"); 
-function callbackFunc() {
-  for (var i = 0; i < items.length; i++) {
-    if (isElementInViewport(items[i])) {
-      items[i].classList.add("in-view");
-    }
+   
+const sections = qs('.section', true);
+const timeline = qs('.timeline');
+const line = qs('.line');
+line.style.bottom = `calc(100% - 20px)`;
+let prevScrollY = window.scrollY;
+let up, down;
+let full = false;
+let set = 0;
+const targetY = window.innerHeight * .8;
+   
+function scrollHandler(e) {
+  const {
+      scrollY
+  } = window;
+  up = scrollY < prevScrollY;
+  down = !up;
+  const timelineRect = timeline.getBoundingClientRect();
+  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
+  
+  const dist = targetY - timelineRect.top;
+  console.log(dist);
+  
+  if (down && !full) {
+      set = Math.max(set, dist);
+      line.style.bottom = `calc(100% - ${set}px)`;
   }
+   
+  if (dist > timeline.offsetHeight + 50 && !full) {
+      full = true;
+      line.style.bottom = `-50px`;
+  }
+  
+  sections.forEach(item => {
+  // console.log(item);
+      const rect = item.getBoundingClientRect(); //     console.log(rect);
+   
+      if (rect.top + item.offsetHeight / 5 < targetY) {
+          item.classList.add('show-me');
+      }
+  }); // console.log(up, down); 
+  
+  prevScrollY = window.scrollY;
 }
- 
-window.addEventListener("load", callbackFunc);
-window.addEventListener("scroll", callbackFunc);
+   
+scrollHandler();
+line.style.display = 'block';
+window.addEventListener('scroll', scrollHandler);
