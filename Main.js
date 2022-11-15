8a2686c39212aee95e55bea1741f9b79b40f20e9
dan-011 <daniel.paliulis@uconn.edu>
2022-11-15 00:03:59 -0500

    terminal-style name mk1

--- Main.js
+++ Main.js
@@ -56,3 +56,45 @@ function scrollHandler(e) {
 scrollHandler();
 line.style.display = 'block';
 window.addEventListener('scroll', scrollHandler);
+
+terminalAnimation('Daniel Paliulis', 'text','white');
+
+
+function terminalAnimation(word, id, color){
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id);
+  target.setAttribute('style', 'color:' + color);
+  window.setInterval(function(){
+    if(letterCount == 0 && waiting == false){
+      waiting = true;
+      window.setTimeout(function(){
+        x = 1;
+        letterCount += x;
+        waiting = false;
+      }, 1000);
+    }
+    else if(letterCount == word.length + 1 && waiting == false){
+      waiting = true;
+    }
+    else if(waiting == false){
+      target.innerHTML = word.substring(0, letterCount);
+      letterCount += x;
+    }
+  }, 120);
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
